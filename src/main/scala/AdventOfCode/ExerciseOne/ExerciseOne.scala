b7874da292914dd5074c022065a08aae6cfc2f99
mywyau <47248104+mywyau@users.noreply.github.com>
2020-12-09 16:59:30 +0000

    advent of code two

--- src/main/scala/AdventOfCode/ExerciseOne/ExerciseOne.scala
+++ src/main/scala/AdventOfCode/ExerciseOne/ExerciseOne.scala
@@ -10,9 +10,7 @@ object ExerciseOne extends App {
 
   val collectionsOfNumbersThatAddToRemainder: Seq[Seq[Int]] = adventOfCode.remainders.map(i => adventOfCode.remainderMinusOriginalNumbersCollectPositive(i))
   val filterCloudsBasedOnOriginals: Seq[Seq[Int]] = collectionsOfNumbersThatAddToRemainder.map(xs => xs.flatMap(x => exerciseOneNumbers.filter(_ == x)))
-  val filterEmptyClouds: Seq[Seq[Int]] = {
-    filterCloudsBasedOnOriginals.filterNot(_.isEmpty)
-  }
+  val filterEmptyClouds: Seq[Seq[Int]] = filterCloudsBasedOnOriginals.filterNot(_.isEmpty)
   // these numbers add to 2020 - x, to determine the numbers in the original list that add to x
   // filterCloudsBasedOnOriginals gets back the two numbers which add to the remainder that are also present in the original sequence together you can deduce
   // the 3 numbers which add to 2020, so go ahead and multiply them all to get exercise 1 part 2 answer
--- src/main/scala/AdventOfCode/ExerciseTwo/AdventOfCodeTwo.scala
+++ src/main/scala/AdventOfCode/ExerciseTwo/AdventOfCodeTwo.scala
@@ -1,41 +1,105 @@
 package AdventOfCode.ExerciseTwo
 
-import scala.util.matching.Regex
+case class PasswordModel(min: Int, max: Int, letter: String, password: String) {
 
-case class PasswordModel(min: Int, max: Int, letter: String, password: String)
+  def validation: Boolean = {
+    val letterKeyCount = password.count(_ == letter.charAt(0))
+    if (letterKeyCount >= min && letterKeyCount <= max) {
+      true
+    } else {
+      false
+    }
+  }
+
+}
 
 class AdventOfCodeTwo {
 
-  val passwords: Seq[String] = AdventOfCodeTwoPasswords.passwords
+  val listOfPasswords: Seq[String] = AdventOfCodeTwoPasswords.passwords
 
   val numberPattern: String = "^\\d+-\\d+" //only the beginning number-number
+  val minPattern: String = "^\\d+-" //only the min number and hyphen
+  val maxPattern: String = "-\\d+$" //only the min number and hyphen
   val passwordPattern: String = "[a-z]+$" //only letter based passwords
   val letterKeyRegex: String = " [a-z]: " //only letterKey
 
   val wholePassword: String = "^\\d+-\\d+ [a-z]: [a-z]*$" //entire password
 
-  def getNumberPart(password: String) = password.replaceAll(passwordPattern, "").replaceAll(letterKeyRegex, "")
+  def getNumberPart(password: String): String = {
+    password.replaceAll(passwordPattern, "").replaceAll(letterKeyRegex, "")
+  }
 
-  def getLetterKey(password: String) = password.replaceAll(numberPattern, "").replaceAll(passwordPattern, "").trim.replace(":", "")
+  def getMin(numberKey: String): Int = numberKey.replaceAll(maxPattern, "").toInt
 
-  def getPasswordPart(password: String) = password.replaceAll(numberPattern, "").replaceAll(letterKeyRegex, "")
+  def getMax(numberKey: String): Int = numberKey.replaceAll(minPattern, "").toInt
 
-  //  def passWordModelCreator(password: String): PasswordModel =  PasswordModel(password)
+  def getLetterKey(password: String): String = {
+    password.replaceAll(numberPattern, "")
+      .replaceAll(passwordPattern, "")
+      .trim.replace(":", "")
+  }
 
+  def getPasswordPart(password: String): String = password.replaceAll(numberPattern, "").replaceAll(letterKeyRegex, "")
+
+  val minMaxList: Seq[String] = listOfPasswords.map(data => getNumberPart(data))
+  val minList: Seq[Int] = minMaxList.map(data => getMin(data))
+  val maxList: Seq[Int] = minMaxList.map(data => getMax(data))
+  val letterList: Seq[String] = listOfPasswords.map(data => getLetterKey(data))
+  val passwordList: Seq[String] = listOfPasswords.map(data => getPasswordPart(data))
+
+  def passwordModelCreator(): Seq[PasswordModel] = {
+
+    val zippedTogether: Seq[(Int, Int, String, String)] = minList zip maxList zip letterList zip passwordList map {
+      case (((min, max), key), password) => (min, max, key, password)
+    }
+
+    for {
+      passwordData <- zippedTogether
+    } yield PasswordModel(passwordData._1, passwordData._2, passwordData._3, passwordData._4)
+  }
 
 }
 
 object ExerciseTwo extends App {
 
   val passwords: Seq[String] = AdventOfCodeTwoPasswords.passwords
+  val totalNumberOfPasswords: Int = AdventOfCodeTwoPasswords.passwords.size
 
   val model = new AdventOfCodeTwo
 
-  val numbers = model.getNumberPart("1-7 q: qqqqxvqrkbqqztlqlzq")
+  val numbers = model.getNumberPart("1-7 q: qqqqxvqrkbqqztlqlzq") //test the string strippers to get the desired components: min, max, key, password
   val password = model.getPasswordPart("1-7 q: qqqqxvqrkbqqztlqlzq")
-  val key = model.getLetterKey("1-7 q: qqqqxvqrkbqqztlqlzq")
+  val letterKey = model.getLetterKey("1-7 q: qqqqxvqrkbqqztlqlzq")
 
-  println(numbers)
-  println(password)
-  println(key)
+  val minKey = model.getMin(numbers)
+  val maxKey = model.getMax(numbers)
+
+  val minList = model.minList
+  val maxList = model.maxList
+  val letterList = model.letterList
+  val passwordList = model.passwordList
+
+  val minListSize = model.minList.size //all lists size == 1000
+
+  val seqOfPasswordModels = model.passwordModelCreator()
+
+  val seqOfValidatedPasswords: Seq[Boolean] = seqOfPasswordModels.map(_.validation)
+  val removeAllInvalidPasswords: Seq[Boolean] = seqOfValidatedPasswords.filter(_ == true)
+  val sizeOfValidPasswordCollection = removeAllInvalidPasswords.length
+
+
+  println(s"The total number of passwords = $totalNumberOfPasswords")
+  println(s"Test number key with hyphen: $numbers")
+  println(s"for password $password the min key: $minKey")
+  println(s"for password $password the min key: $maxKey")
+  println(s"Test key: $letterKey")
+  println(s"Test password: $password")
+  println(s"number of min key = $minListSize")
+  println(model.passwordList) // seq iof data only password part
+  println(seqOfPasswordModels) // password data is now wrapped in Password case classes
+  println(seqOfValidatedPasswords)  //applied the validation to seq of Password models
+                                    // to tell me if password passes the validation given in requirements, return trues and falses
+
+  println(removeAllInvalidPasswords) //filter and collect only the true
+  println(sizeOfValidPasswordCollection) //return the size of the list of valid passwords (true)
 }
