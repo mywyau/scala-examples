83397c3b529ee8c71be075c5c98d254862b571cb
mywyau <47248104+mywyau@users.noreply.github.com>
2021-01-24 23:02:04 +0000

    more content

--- src/main/scala/topics/futures/TimeLord.scala
+++ src/main/scala/topics/futures/TimeLord.scala
@@ -5,8 +5,17 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class TimeLord()() {
 
-  val futureA: Seq[Future[String]] = ???
+  /*
+    Oh noes you have a Sequence of Futures :( this may not be the type signature you want.
+    Individual futures in the seq can fail and you will need to handle this
+ */
+  val sequenceOfFutures: Seq[Future[String]] = ???
 
+  /*
+    If we want a Future[Seq[A]] type from our 'val sequenceOfFutures: Seq[Future[String]] = ???',
+    We will shuffle 'sequenceOfFutures' into the desired type Future[Seq[A]] using 'Future.sequence()'
+  */
+  val reshuffleSequenceOfFutures: Future[Seq[String]] = Future.sequence(sequenceOfFutures)
 
 
 }
--- src/main/scala/topics/polymorphism/Polymorph.scala
+++ src/main/scala/topics/polymorphism/Polymorph.scala
@@ -1,12 +1,19 @@
 package topics.polymorphism
 
+import scala.reflect.ClassTag
+
 class Polymorph {
+
+  /*
+    We will pass in a generic type A that will be then matched on and extracted out based on the type passed into the method
+    We need to explicitly do this for each type scenario.
+  */
 
   def applyDiscount[A](discount: A): String = {
     discount match {
       case d: String => s"Lookup percentage discount in database for: $d"
       case d: Int => s"$d discount will be applied"
-      case _ => "Unsupported discount type"
+      case _ => "Unsupported discount type" // wild card to be safe ;)
     }
   }
 
@@ -24,5 +31,47 @@ class Polymorph {
     }
   }
 
+  /*
+   There is a scenario you may want with custom types and only collect based on your custom enum, however Scala has type erasure at
+   compile time so you need to wrap your values in a 'ClassTag'. Another concept is TypeTags look them up when needed.
+   ClassTags can be a little dangerous when debugging as it the object type is overwritten and can be hidden, use only
+   when necessary. If used safely then it should be fine, remember to document and make things clear that there is a ClassTag involved,
+   and you are overriding the type erasure.
+ */
+
+  sealed trait SelectClaimPeriod {
+    val somethingSelectClaimPeriodOnly = ???
+  }
+
+  sealed trait NovemberOnwards extends SelectClaimPeriod {
+    val somethingNovemberOnwardsOnly = ???
+  }
+
+  case object November extends NovemberOnwards
+
+  sealed trait DecemberOnwards extends NovemberOnwards {
+    val somethingDecemberOnwardsOnly = ???
+  }
+
+  case object December extends DecemberOnwards
+
+  sealed trait JanuaryOnwards extends DecemberOnwards {
+    val somethingJanuaryOnwardsOnly = ???
+  }
+
+  case object January extends JanuaryOnwards
+
+  case object February extends JanuaryOnwards
+
+  case object March extends JanuaryOnwards
+
+
+  val allValues: Seq[SelectClaimPeriod] = Seq(November, December, January, February, March)
+
+  val notNovemberMonths: Seq[DecemberOnwards] = allValues.collect { case month: DecemberOnwards => month }
+
+  def onwardsValues[T]()(implicit tag: ClassTag[T]): Seq[T] = {
+    notNovemberMonths.collect[T, Seq[T]] { case month: T => month }
+  }
 
 }
