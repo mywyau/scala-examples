815c5188c038173f0e60dd9aaeeb9b835d45cea0
mywyau <47248104+mywyau@users.noreply.github.com>
2021-04-27 20:52:20 +0100

    refactored ap example

---
+++ src/main/scala/learnCatsAndStuff/validated/liefBattermann/UserValidationError.scala
@@ -0,0 +1,10 @@
+package learnCatsAndStuff.validated.liefBattermann
+
+sealed trait UserValidationError
+
+case object NameNotValid extends UserValidationError
+
+case object AgeOutOfRange extends UserValidationError
+
+case object EmailNotValid extends UserValidationError
+
