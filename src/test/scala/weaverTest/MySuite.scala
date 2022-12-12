3c773f3b7eac86a63952aa1e4aa18ba23f149ffb
mywyau <47248104+mywyau@users.noreply.github.com>
2022-12-12 16:08:35 +0000

    fix dependency and test parallel runner

--- src/test/scala/weaverTest/MySuite.scala
+++ src/test/scala/weaverTest/MySuite.scala
@@ -1,22 +1,39 @@
 package weaverTest
 
-import weaver.SimpleIOSuite
 import cats.effect._
+import weaver.SimpleIOSuite
+
+import java.util.UUID
 
 // Suites must be "objects" for them to be picked by the framework
 object MySuite extends SimpleIOSuite {
 
-  pureTest("non-effectful (pure) test") {
-    expect("hello".length == 6)
-  }
+//  pureTest("non-effectful (pure) test") {
+//    expect("hello".length == 6)
+//  }
 
-  private val random = IO(java.util.UUID.randomUUID())
+  private val random: IO[UUID] = IO(java.util.UUID.randomUUID())
+
+  private val sleepOneSecondIO: IO[Unit] = IO(Thread.sleep(1000))
+
+  private val sleepOneSecondIOThenGenerateRandomValue: IO[UUID] = IO(Thread.sleep(1000)).flatMap(_ => random)
 
   test("test with side-effects") {
     for {
       x <- random
       y <- random
     } yield expect(x != y)
+  }
+
+  for (n <- 1 to 100) {
+    test(s"Scenario $n out of 100 - test with sleeps") {
+      for {
+        x <- sleepOneSecondIOThenGenerateRandomValue
+//        _ <- sleepOneSecondIO
+        y <- sleepOneSecondIOThenGenerateRandomValue
+      } yield
+        expect(x != y)
+    }
   }
 
   loggedTest("test with side-effects and a logger") { log =>
